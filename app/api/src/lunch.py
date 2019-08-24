a180f87a8fcf296bd2039aea17cbee5085d4b33a
Roman-F <roman_11_11_11@mail.ru>
2019-08-24 21:23:48 +0700

    Рут и src  для lunch

---
+++ app/api/src/lunch.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from app.api.sql.lunch_provider import Provider
+
+
+def base_func(provider, user_data):
+    '''
+    Инкапсуляция общей логики вызова провайдера и обработки ответа
+    :provider: провадер (функция, которую планируем вызывать)
+    :user_data: данные
+    '''
+    answer = provider(user_data)
+    if isinstance(answer, list):
+        answer = answer[0]
+    return answer
+
+def lunch_get_all_id(user_data):
+
+    provider = Provider()
+    return base_func(provider.get_all_id, user_data)
+
+def lunch_get_active_id(user_data):
+
+    provider = Provider()
+    return base_func(provider.get_active_id, user_data)
+
--- app/route/route_list.py
+++ app/route/route_list.py
@@ -2,11 +2,13 @@ from app.route.route_auth import Auth
 from app.route.route_profile import Profile
 from app.route.route_register import Register
 from app.route.route_info_user import InfoUser
-
+from app.route.route_lunch import Lunch, LunchAll
 
 ROUTES = {
     Register: '/register',
     Auth: '/auth',
     Profile: '/profile/<int:id_user>',
     InfoUser: '/info/<int:id_nom>/<string:id_user>',
+    Lunch: '/lunch',
+    LunchAll: '/lunch/all',
 }
