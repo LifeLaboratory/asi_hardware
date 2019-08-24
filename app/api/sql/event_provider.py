6850f59568dbca32dc0763e9475dde9e204bc5ff
Orange-7 <alexandroff.m@gmail.com>
2019-08-25 01:12:58 +0700

    добавлены event

--- app/api/sql/event_provider.py
+++ app/api/sql/event_provider.py
@@ -8,17 +8,51 @@ class Provider:
         query = """
     select
       event_id,
-      Title title,
-      Place place,
-      Photo photo,
-      Description description,
-      Date date
+      title,
+      place,
+      photo,
+      description,
+      "date"
     from event
                 """
         return Sql.exec(query=query, args=args)
 
-
     @staticmethod
     def set_event(args):
         """Добавление нового ивента"""
-        pass
+        query = """
+    insert into event (
+      title,
+      place,
+      photo,
+      description,
+      "date"
+    )
+    values (
+      {title},
+      {place},
+      {photo},
+      {description},
+      {date}
+    )
+                """
+        return Sql.exec(query=query, args=args)
+
+    @staticmethod
+    def put_event(args):
+        """
+        запрос на обновление ивента
+        :param args:
+        :return:
+        """
+        query = """
+    update event 
+    set
+      title = {title},
+      place = {place},
+      photo = {photo},
+      description = {description}, 
+      "date" = {date}
+    where event_id = {eventId}
+                """
+        return Sql.exec(query=query, args=args)
