13dc8b1ee415587a727cc141e3192a7d52f053bf
Roman-F <roman_11_11_11@mail.ru>
2019-08-24 22:39:51 +0700

    Рут и src  для Event Небольшой рефакторинг для profile

--- app/api/src/profile.py
+++ app/api/src/profile.py
@@ -2,6 +2,9 @@ from app.api.sql.profile_provider import Provider
 
 
 def get_profile(args):
+
     provider = Provider()
-    answer = provider.get_profile(args)[0]
+    answer = provider.get_profile(args)
+    if isinstance(answer, list):
+        answer = answer[0]
     return answer
--- app/route/route_list.py
+++ app/route/route_list.py
@@ -3,6 +3,7 @@ from app.route.route_profile import Profile
 from app.route.route_register import Register
 from app.route.route_info_user import InfoUser
 from app.route.route_lunch import Lunch, LunchAll
+from app.route.route_event import Event, EventAll
 
 ROUTES = {
     Register: '/register',
@@ -11,4 +12,6 @@ ROUTES = {
     InfoUser: '/info/<int:id_nom>/<string:id_user>',
     Lunch: '/lunch',
     LunchAll: '/lunch/all',
+    Event: '/event',
+    EventAll: '/event/all',
 }
